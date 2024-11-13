13a942e653330f93d538495894bf38dba2ef372b
aminu <siramin.aa@gmail.com>
2024-11-13 18:07:42 +0100

    complete and exit button implemented

--- gui.py
+++ gui.py
@@ -6,7 +6,11 @@ input_box = sg.InputText(tooltip="Enter todo",key='todo')
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',enable_events=True,size=[45,10])
 edit_button = sg.Button('Edit')
-window = sg.Window('MY TO-DO APP',layout=[[label],[input_box],[add_button],[[list_box,edit_button]]],
+complete_button = sg.Button('Complete')
+exist_button = sg.Button('Exit')
+window = sg.Window('MY TO-DO APP',layout=[[label],
+                                          [input_box,add_button],
+                                          [[list_box,edit_button, complete_button]],[exist_button]],
                    font=('Helvetica',20))
 while True:
 
@@ -31,6 +35,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
